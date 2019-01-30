b5b429c65b1922465048bd99139989d6b24e0ffc
Jaume Marhuenda <jaumemarhuenda@gmail.com>
2019-01-29 17:49:07 -0800

    BUG-117239: Spark HWC Timestamp is NULL
    Change-Id: Ieb96449a13f372105f16075945ff152c4cf7c823

--- src/test/java/com/hortonworks/spark/sql/hive/llap/HiveQlUtilTest.java
+++ src/test/java/com/hortonworks/spark/sql/hive/llap/HiveQlUtilTest.java
@@ -1,11 +1,14 @@
 package com.hortonworks.spark.sql.hive.llap;
 
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.catalyst.expressions.GenericInternalRow;
+import org.apache.spark.sql.catalyst.expressions.UnsafeRow;
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.unsafe.types.UTF8String;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static com.hortonworks.spark.sql.hive.llap.util.HiveQlUtil.formatRecord;
@@ -33,5 +36,21 @@ public class HiveQlUtilTest extends SessionTestBase {
 
     formatted = formatRecord(schema, record, "\\", "'");
     assertTrue(Arrays.equals(new Object[]{0, UTF8String.fromString("'h\\,w\\,c'"), null}, formatted));
+  }
+
+  @Test
+  public void testTimestampSchemaUnsafeRow() {
+    byte[] buffer = new byte[100];
+    UnsafeRow record = new UnsafeRow(2);
+    record.pointTo(buffer, 100);
+    record.setInt(0, 1);
+    record.setLong(1, 1548811552012000L);
+
+    StructType timeSchema = new StructType()
+        .add("c1", "int")
+        .add("c2", "timestamp");
+
+    Object[] formatted = formatRecord(timeSchema, record, null, null);
+    Assert.assertEquals(new Object[]{1, UTF8String.fromString("2019-01-29 17:25:52.012")}, formatted);
   }
 }
